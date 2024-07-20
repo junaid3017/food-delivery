41514f4b8a69d53b44f0c36380b0e129d2c18a98
Junaid Hussain <junaid11hussain22@female.com>
2024-07-20 10:26:00 +0530

    Added a change

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className='header'>
       <div className="header-contents">
         <h2>Order yout favourite food here </h2>
-        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Out mission is to satisfy your cravings and elevate yout dining experience, one delicious meal at a time.</p>
+        <p>Choose from a diverse a menu featuring  delectable array of dishes crafted with the finest ingredients and culinary expertise. Out mission is to satisfy your cravings and elevate yout dining experience, one delicious meal at a time.</p>
         <button>View Menu</button>
       </div>
     </div>
