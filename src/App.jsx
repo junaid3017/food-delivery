c1d1708db27ed5d20d8c4934c4c38a6c99f8a627
Junaid Hussain <junaid11hussain22@female.com>
2024-07-20 23:31:25 +0530

    Add Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
+import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+// import Home from './pages/Home/Home'
+import { Route, Router, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+// import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 const App = () => {
   return (
     <div className='app'>
     <Navbar/>
+    
+     
+     
       <Routes>
+      
         <Route path='/' element={<Home/>}/>
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/order' element={<PlaceOrder/>}/>
       </Routes>
+      
+     
     </div>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='header'>
       <div className="header-contents">
-        <h2>Order yout favourite food here </h2>
+        <h2>Order your favourite food here </h2>
         <p>Choose from a diverse a menu featuring  delectable array of dishes crafted with the finest ingredients and culinary expertise. Out mission is to satisfy your cravings and elevate yout dining experience, one delicious meal at a time.</p>
         <button>View Menu</button>
       </div>
